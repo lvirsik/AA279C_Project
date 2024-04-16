3de7afbe446f9c432f3c5abaf3bc8e4c42201f40
Steven <stevenstmai@gmail.com>
2024-04-16 15:53:55 -0700

    need boldholde

--- Graphics/visualizeEllipsoids.py
+++ Graphics/visualizeEllipsoids.py
@@ -4,12 +4,92 @@ from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import cm
 from matplotlib.image import imread
 from Simulator.simulationConstants import *
+from Vehicle.satelliteConstants import *
 
-# Plot the possible trajectory the angular velocity can trace with the constraint of being compatible with the energy constant
-def plot_w_Energy(trajectory):
+def get_w_from_EulerAngle(trajectory):
+     # 1 2 3 = phi theta psi = yaw pitch roll
+    euler_angles = trajectory [6:9]
+    euler_angle_rates = trajectory[9:12]
+
+    phi = euler_angles[0]
+    theta = euler_angles[1]
+    psi = euler_angles[0]
+    
+    phiDot = euler_angle_rates[0]
+    thetaDot = euler_angle_rates[1]
+    psiDot = euler_angle_rates[2]
+
+    # Angular velocity from euler angles and euler angles rates
+    w = np.array([[phiDot * np.sin(theta) * np.sin(phiDot) + thetaDot * np.cos(psi)],
+                  [phiDot * np.sin(theta) * np.cos(phiDot) + thetaDot * np.sin(psi)],
+                  [phiDot * np.cos(theta) + psiDot]])
+    return w
+
+# Plot the possible trajectory the angular velocity can trace with the constraint of being compatible with the energy content and magnitude of the anguler momentum
+def plot_w_Energy_Momentum(init_c):
+    w = init_c[6:9]
+
+    I_x = I_COM_BF[0,0]
+    I_y = I_COM_BF[1,1]
+    I_z = I_COM_BF[2,2]
+
+    # Get rid of 0.5 on right side and have 2 on left side 2T = 1/2 * I * w^2 
+    KE = np.dot(w, np.dot(I_COM_BF, w))
+    print(KE)
+
+    # Ellipsoid axis
+    a = pow((KE / I_x), 0.5)
+    b = pow((KE / I_y), 0.5)
+    c = pow((KE / I_z), 0.5)
+    print(a)
+
+    # Plotting ellipsoid using spherical coordinates
+    ellipsoid_phi = np.linspace(0,2*np.pi, 256).reshape(256, 1)
+    ellipsoid_theta = np.linspace(0, np.pi, 256).reshape(-1, 256)
+    x = a*np.sin(ellipsoid_theta)*np.cos(ellipsoid_phi)
+    y = b*np.sin(ellipsoid_theta)*np.sin(ellipsoid_phi)
+    z = c*np.cos(ellipsoid_theta)
+
+    # Plot trajectory in 3D
+    fig = plt.figure(1)
+    ax = fig.add_subplot(111, projection='3d')
+    print("before1")
+    ax.plot_surface(x, y, z)
+    print("before2")
+    plt.show()
+    print("after")
 
 # Plot the possible trajectory the angular velocity can trace with the constraint of being compatible with the magnitude of Angular Momentum
-def plot_w_Momentum(trajectory):
+def plot_w_Momentum(init_c):
+    w = init_c[6:9]
 
-# Plot the possible trajectory the angular velocity that is within both the energy ellipsoid and the moomentum ellipsoid, a bolhode.
-def plot_polHode(trajectory):
+    I_x = I_COM_BF[0,0]
+    I_y = I_COM_BF[1,1]
+    I_z = I_COM_BF[2,2]
+
+    # Get rid of 0.5 on right side and have 2 on left side 2T = 1/2 * I * w^2 
+    L = np.dot(I_COM_BF, w)
+    # L_sqr = pow(L, 2)
+    print(L)
+
+    # Ellipsoid axis
+    a = L / I_x
+    b = L / I_y
+    c = L / I_z
+    print(a)
+
+    # Plotting ellipsoid using spherical coordinates
+    ellipsoid_phi = np.linspace(0,2*np.pi, 256).reshape(256, 1)
+    ellipsoid_theta = np.linspace(0, np.pi, 256).reshape(-1, 256)
+    x = a*np.sin(ellipsoid_theta)*np.cos(ellipsoid_phi)
+    y = b*np.sin(ellipsoid_theta)*np.sin(ellipsoid_phi)
+    z = c*np.cos(ellipsoid_theta)
+
+    # Plot trajectory in 3D
+    fig = plt.figure(2)
+    ax = fig.add_subplot(111, projection='3d')
+    ax.plot_surface(x, y, z)
+    plt.show()
+
+# Plot the possible trajectory the angular velocity that is within both the energy ellipsoid and the momentum ellipsoid, a bolhode.
+#def plot_polHode(trajectory):
--- runSimulation.py
+++ runSimulation.py
@@ -3,14 +3,19 @@ from Simulator.simulation import Simulation
 from Simulator.simulationConstants import *
 from Simulator.dynamics import *
 from Graphics.visualizeOrbits import *
+from Graphics.visualizeEllipsoids import *
 
 
 # Setup Simulation
 initial_cartesian_state = OE_2_ECI(INITIAL_OEs)
-initial_rotational_state = np.array([0, 0, 0, 0, 0, 0])
+initial_rotational_state = np.array([1, 1, 1, 0, 0, 0])
 initial_state = np.concatenate((initial_cartesian_state, initial_rotational_state))
 sim = Simulation(FINAL_TIME, TIMESTEP, initial_state)
 
 # Run Simulation
 trajectory = sim.propogate()
-plot_orbit(trajectory)
+# plot_orbit(trajectory)
+print("1")
+plot_w_Energy_Momentum(initial_state)
+print("2")
+plot_w_Momentum(initial_state)
