9a7d7affc40f1bea388632bd63c13270e3c5fc78
Steven Mai <stevenstmai@gmail.com>
2024-04-17 00:03:57 -0700

    Cleaned up code and their appears to be some functionality for polhode now

--- runSimulation.py
+++ runSimulation.py
@@ -8,13 +8,16 @@ from Graphics.visualizeEllipsoids import *
 
 # Setup Simulation
 initial_cartesian_state = OE_2_ECI(INITIAL_OEs)
-initial_rotational_state = np.array([1, 5, 3, 1, 1, 0])
+initial_rotational_state = np.array([0, 0, 0, 0.1, 0.1, 0.1])
 initial_state = np.concatenate((initial_cartesian_state, initial_rotational_state))
 sim = Simulation(FINAL_TIME, TIMESTEP, initial_state)
 
 # Run Simulation
 trajectory = sim.propogate()
-plot_orbit(trajectory)
+# print(np.shape(trajectory))
+# print(trajectory)
+# plot_orbit(trajectory)
 plot_w_Energy_Momentum(initial_state)
 plot_polHode(initial_state, trajectory)
+
 plt.show()
