9a2e052ee43315172730be3dcfa7df1810525781
Charlie Gordon <charlieg@stanford.edu>
2024-05-01 21:13:32 -0700

    ps4-p4 updates

--- Simulator/simulationConstants.py
+++ Simulator/simulationConstants.py
@@ -8,4 +8,4 @@ FINAL_TIME = 15*24*3600
 TIMESTEP = 10
 INITIAL_OEs = [1432000000, 0.9, np.pi/2, np.pi/2, 0.1, 0.1]
 INITIAL_Q = [1, 0, 0, 1] #will get normalized
-INITIAL_w = np.array([0, 0.000001, 0]) #in Body Frame
+INITIAL_w = np.array([0, 0.00001, 0]) #in Body Frame
